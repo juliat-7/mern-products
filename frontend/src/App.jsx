3cc06fe2e69494c2952e9ddc368c871c573cc0e0
juliat-7 <juliatthomaz@gmail.com>
2025-04-27 12:48:52 +0530

    starte theming

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,14 +9,13 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#1976d2',
+      main: '#182335',
     },
     secondary: {
-      main: '#dc004e',
+      main: '#E15546',
     },
   },
 });
-
 function App() {
   
   return (
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,23 +4,25 @@ import Stack from '@mui/material/Stack';
 import { Link } from 'react-router';
 import { MdOutlineAddBox } from "react-icons/md";
 import { LuMoon } from "react-icons/lu";
-import { Button } from '@mui/material';
+import { Button,useTheme } from '@mui/material';
 
 
 const Navbar = () => {
+  const theme = useTheme()
+  const palette = theme.palette
   return (
-    <Box component="nav" style={{display:'flex',alignItems:'center',justifyContent:'space-around'}}>
+    <Box component="nav" sx={{display:'flex',alignItems:'center',justifyContent:'space-around'}}>
       <Link to="/" style={{ textDecoration: 'none', color: 'inherit', marginLeft:'30px' }}>
         <h1>Products</h1>
       </Link> 
-      <Stack direction={"row"} spacing={1} sx={{ml:'auto'}}>
+      <Stack direction={"row"} spacing={1} sx={{ml:'auto',mr:2}}>
         <Link to="/create">
-        <Button><MdOutlineAddBox style={{ fontSize: '25px',color:"black" }}/></Button>
+        <Button color='primary'><MdOutlineAddBox sx={{ fontSize: '25px',color:palette.primary.main }}/></Button>
         </Link>
-        <Button><LuMoon style={{ fontSize: '25px',color:"black"}}/></Button>
+        <Button><LuMoon sx={{ fontSize: '25px',color:palette.primary.main}}/></Button>
       </Stack>
     </Box>
-  )
+  ) 
 }
 
 export default Navbar
