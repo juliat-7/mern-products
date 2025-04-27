05eabb16e7ccc4a546b1fbc440250795aac7cfbb
juliat-7~ <juliatthomaz@gmail.com~>
2025-04-27 17:57:20 +0530

    navbar done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,10 @@
  import React from 'react'
-import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
 import { Link } from 'react-router';
-import { Button ,IconButton,Typography} from '@mui/material';
+import { Button ,IconButton,Typography,Stack,Box} from '@mui/material';
 import { useTheme } from '@emotion/react';
 import { MdOutlineAddBox } from "react-icons/md";
 import { FiSun } from "react-icons/fi";
+import { FiMoon } from "react-icons/fi";
 import {ColorModeContext} from "../App.jsx"
 
 
@@ -15,14 +14,10 @@ const Navbar = () => {
   const colorMode = React.useContext(ColorModeContext);
   console.log(theme, colorMode)
   return (
-    <Box component="nav" sx={{display:'flex',alignItems:'center',justifyContent:'space-between' }}    >
+    <Box component="nav" sx={{display:'flex',alignItems:'center',justifyContent:'space-between'}}  p={2}   >
 
 
-      <Typography
-        variant="h4"
-        color="primary"
-        component={Link}
-        to="/"
+      <Typography variant="h4" color="primary" component={Link} to="/"
         sx={{
           textDecoration: 'none',
           marginLeft: '30px',
@@ -35,12 +30,12 @@ const Navbar = () => {
       </Typography>
 
       <Stack direction={"row"} spacing={1} >
-        <Link to="/create">
-        <Button variant="outlined" size="large" > <MdOutlineAddBox /> </Button>
-        </Link>
-          <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} >
-            {theme.palette.mode === 'dark' ? <FiSun/> : <h1>hi</h1>}
-          </IconButton>
+        <IconButton color="primary" size="medium" component={Link}to="/create"> 
+          <MdOutlineAddBox /> 
+        </IconButton>
+        <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode}  color="primary" size="medium">
+          {theme.palette.mode === 'dark' ? <FiSun/> : <FiMoon/>}
+        </IconButton>
       </Stack>
     </Box>
   ) 
